4151d4748d0e2c6ba338a5effac379da9374ce70
Shahab Dogar <shahab@dogar.dev>
2026-03-08 20:47:42 +0500

    feat: flake update March 2026 (switch to channel 25.11)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS system flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
     home-manager = {
       url = "github:nix-community/home-manager";
